e033b94c6d4e9ac224aabb0ce94c8e5d0dccdda9
emanmoq <42995029+emanmoq@users.noreply.github.com>
2021-02-25 15:16:56 -0800

    responsive

--- assets/js/main.js
+++ assets/js/main.js
@@ -52,7 +52,6 @@ jQuery(function ($) {
   $('.bookSlider').owlCarousel({
     autoplay: true,
     loop: false,
-    margin: 15,
     rtl:true,
     nav:true,
     dots:true,
@@ -60,23 +59,29 @@ jQuery(function ($) {
     responsive: {
       0: {
         items: 1,
+        margin: 0,
 
       },
       600: {
         items: 2,
+        margin: 5,
+
       },
       1000: {
-        items: 3,
+        items: 2,
+        margin: 15,
+      },
+      1200:{
+        items:3,
+        margin: 15,
 
       }
-      
 
     }
   });
   $('.blogslider').owlCarousel({
     autoplay: true,
     loop: false,
-    margin: 15,
     rtl:true,
     nav:true,
     dots:true,
@@ -84,15 +89,24 @@ jQuery(function ($) {
     responsive: {
       0: {
         items: 1,
+        margin:0,
 
       },
       600: {
         items: 2,
       },
       1000: {
-        items: 3,
+        items: 2,
+        margin: 15,
+
+
+      },
+      1200:{
+        items:3,
+        margin: 15,
 
       }
+
       
 
     }
