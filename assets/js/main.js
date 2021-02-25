b99134c52a277bed14306fbbb18642adf9c09435
emanmoq <42995029+emanmoq@users.noreply.github.com>
2021-02-25 06:04:09 -0800

    writer

---
+++ assets/js/main.js
@@ -0,0 +1,132 @@
+jQuery(function ($) {
+
+
+  function onScroll(event){
+			//add class active when click on navbar item
+		var scrollPosition = $(document).scrollTop();
+		$('.navbar-collapse .nav-item a[href^="#"').each(function () {
+			console.log($(this).attr('href')); 
+		  var refElement = $($(this).attr("href"));
+		  if (refElement.length&&refElement.position().top <= scrollPosition) {
+			$('.navbar-collapse .nav-item ').removeClass("active");
+			$(this).parent().addClass("active");
+		  }
+		  else{
+			$(this).removeClass("active");
+		  }
+  
+		});
+	  }
+    $(document).on("scroll", onScroll);
+//animate to section when click
+    $('.navbar-collapse ul li a').on('click', function () {
+      $('html, body').animate({ scrollTop: $(this.hash).offset().top  - 5 }, 1000);
+      $('.navbar-collapse.collapse.in').removeClass('in');
+      return false;
+    });
+        // add fix navbar when scroll down and remove it on top 
+  $(window).scroll(function () {
+    scrollTop = $(window).scrollTop();
+
+    if (scrollTop > $('header').height()) {
+      $('header').addClass('scrollNav');
+
+    }
+
+    else {
+      $('header').removeClass('scrollNav');
+    }
+
+  });
+
+    
+  //close mobile navbar when chooe one of items
+  $(".navbar .nav-link").click(function(){
+    $(".collapse").removeClass("show");
+  });
+  $(".bookmark").click(function(){
+    $('.bookmark').not(this).find(".fa-bookmark").removeClass(' far fa-bookmark').addClass('fas fa-bookmark"');
+    $(this).find(".fa-bookmark").toggleClass('far fa-bookmark fas fa-bookmark');
+  })
+
+  $('.bookSlider').owlCarousel({
+    autoplay: true,
+    loop: false,
+    margin: 15,
+    rtl:true,
+    nav:true,
+    dots:true,
+
+    responsive: {
+      0: {
+        items: 1,
+
+      },
+      600: {
+        items: 2,
+      },
+      1000: {
+        items: 3,
+
+      }
+      
+
+    }
+  });
+  $('.blogslider').owlCarousel({
+    autoplay: true,
+    loop: false,
+    margin: 15,
+    rtl:true,
+    nav:true,
+    dots:true,
+
+    responsive: {
+      0: {
+        items: 1,
+
+      },
+      600: {
+        items: 2,
+      },
+      1000: {
+        items: 3,
+
+      }
+      
+
+    }
+  });
+  $('.quotationSlider').owlCarousel({
+    autoplay: true,
+    loop: false,
+    items:1,
+    rtl:true,
+    nav:false,
+    dots:true,
+  });
+  $('.opinionSlider').owlCarousel({
+    autoplay: true,
+    loop: true,
+    rtl:true,
+    nav:false,
+    dots:false,
+    items:1,
+
+  });
+    // declare rating 
+    $(".my-rating").starRating({
+      starSize: 16,
+      rtl: true,
+      activeColor: '#FFAE01',
+      hoverColor: '#F9D674',
+      ratedColor: '##FFAE01',
+      emptyColor: '#F3F3F3',
+      starShape: 'rounded',
+      useGradient: false,
+      disableAfterRate: false,
+  
+      callback: function (currentRating, $el) { }
+    });
+});
+
